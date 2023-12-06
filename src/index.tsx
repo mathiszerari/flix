f4d9ba60ee63c04386a748de972ea8fb6069fa9a
Val <pro.mvaldo@gmail.com>
2023-12-06 09:47:23 +0100

    🏗️setup react router

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,55 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Shows from './pages/Shows';
+import Calendar from './pages/Calendar';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Homepage from './pages/Homepage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {
+        path:"",
+        element: <Homepage/>
+      },
+      {
+        path: "calendar",
+        element: <Calendar/>
+      },
+
+      {
+        path: "shows/:id",
+        element: <Shows/>
+      },
+      {
+        path: "profile",
+        element: <Profile/>
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/register",
+    element: <Register/>
+  }
+  
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
