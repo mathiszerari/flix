6d007166c70265c4f2851e9f44347f3c0d77f2e9
Val <pro.mvaldo@gmail.com>
2023-12-08 09:23:54 +0100

    💄Home style

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { GenresProvider } from './context/GenresContext';
 
 function App() {
   return (
-    <div className="App px-6 pb-16">
+    <div className="App px-6">
       <GenresProvider>
         <Header />
         <Outlet />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,6 +43,7 @@ export default function Header() {
   }, []);
 
   return (
+<<<<<<< HEAD
     <div>
       <div className="hidden">
         {isLoadingNotification ? (
@@ -51,6 +52,21 @@ export default function Header() {
         ) : (
           // Render the Notification component when the loading is complete
           <Notification getNotification={setIsNotificationOpen} />
+=======
+    <header className="flex items-center justify-between">
+      <NavLink to="/">
+        <img className="w-16" src={flixlogo} alt="flix app logo" />
+      </NavLink>
+      <nav className="flex items-center justify-around mt-4 gap-4">
+        <SearchBar/>
+        <NavLink to="calendar">Calendar</NavLink>
+        {/* Condition pour afficher ou cacher les liens */}
+        {email ? null : (
+          <>
+            <NavLink to="login">Login</NavLink>
+            <NavLink to="signup">Signup</NavLink>
+          </>
+>>>>>>> efeddee (💄Home style)
         )}
       </div>
       <header className="flex items-center justify-between backdrop-blur-lg">
